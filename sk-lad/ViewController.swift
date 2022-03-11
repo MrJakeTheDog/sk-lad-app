a545f9de8d2dba1321c060941f8945144402cbcd
Demchenko <mykhailo.it@gmail.com>
2022-03-11 02:09:58 +0200

    Design signIn and signUp buttons

--- sk-lad/ViewController.swift
+++ sk-lad/ViewController.swift
@@ -11,9 +11,18 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        signInButton.layer.cornerRadius = 19
+        signInButton.layer.borderWidth = 1.2
+        signInButton.layer.borderColor = UIColor(red: 212.0/255.0, green: 226.0/255.0, blue: 218.0/255.0, alpha: 1.0).cgColor
+        signInButton.clipsToBounds = true
+        
+        signUpButton.layer.cornerRadius = 19
+        signUpButton.clipsToBounds = true
     }
 
-
+    @IBOutlet weak var signInButton: UIButton!
+    @IBOutlet weak var signUpButton: UIButton!
+    
 }
 
